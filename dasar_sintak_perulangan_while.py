83f536d319c4a38535d79062c120ee392385c4f0
Fikri Muza <fikrimuzadi@gmail.com>
2023-01-15 22:56:02 +0700

    perulangan

--- dasar_sintak_perulangan_while.py
+++ dasar_sintak_perulangan_while.py
@@ -3,13 +3,30 @@ Program perulangan membaca buku dengan while
 """
 
 jumlah_buku = 10
-jumlah_buku_terbaca = 0
+jumlah_buku_terbaca_dan_dipahami = 0
+total_jumlah_baca = 0
 
 print('Ibu berkata, "Baca semua bukumu"')
-print(f'Jumlah buku yang sudah dibaca {jumlah_buku_terbaca}')
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_terbaca_dan_dipahami}')
 
-while jumlah_buku_terbaca < jumlah_buku:
-    jumlah_buku_terbaca = jumlah_buku_terbaca + 1
-    print(f'Buku ke {jumlah_buku_terbaca} sudah dibaca')
+while total_jumlah_baca < jumlah_buku:
+    print(total_jumlah_baca)
+    total_jumlah_baca +=1
 
-print(f'Jumlah buku yang sudah dibaca{jumlah_buku_terbaca}')
+'''
+while total_jumlah_baca < jumlah_buku * 2:
+    total_jumlah_baca = total_jumlah_baca + 1
+
+    if jumlah_buku_terbaca_dan_dipahami == 9:
+        print(f"Buku ke {jumlah_buku_terbaca_dan_dipahami + 1} belum paham")
+    else:
+        jumlah_buku_terbaca = jumlah_buku_terbaca_dan_dipahami + 1
+        print(f'Buku ke {jumlah_buku_terbaca} sudah dibaca dan dipahami')
+
+print(f'Jumlah buku yang sudah dibaca{jumlah_buku_terbaca_dan_dipahami}')
+if jumlah_buku_terbaca_dan_dipahami == jumlah_buku:
+    print('Bu, semua buku sudah dibaca dan dipahami')
+else:
+    print(f'Bu, tidak semua buku bisa dipahami.'
+          f' budi hanya bisa memahami {jumlah_buku} buku')
+'''
