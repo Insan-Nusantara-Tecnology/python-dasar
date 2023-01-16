1838bbad00f81a8095381f366128fe077ae8383d
Fikri Muza <fikrimuzadi@gmail.com>
2023-01-16 14:28:41 +0700

    perulangan dengan pemberhenti

--- dasar_sintak_perulangan_while.py
+++ dasar_sintak_perulangan_while.py
@@ -3,31 +3,30 @@ Program perulangan membaca buku dengan while
 """
 
 jumlah_buku = 10
-jumlah_buku_terbaca_dan_dipahami = 0
-total_jumlah_baca = 0
+terbaca_dan_dipahami = 0
+jumlah_baca = 0
 
 print('Ibu berkata, "Baca semua bukumu"')
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_terbaca_dan_dipahami}')
+print(f'Jumlah buku yang sudah dibaca dan dipahami {terbaca_dan_dipahami}')
 
+'''
 while total_jumlah_baca < jumlah_buku:
     print(total_jumlah_baca)
     total_jumlah_baca +=1
-
-
 '''
-while total_jumlah_baca < jumlah_buku * 2:
-    total_jumlah_baca = total_jumlah_baca + 1
 
-    if jumlah_buku_terbaca_dan_dipahami == 9:
-        print(f"Buku ke {jumlah_buku_terbaca_dan_dipahami + 1} belum paham")
+while jumlah_baca < jumlah_buku * 2:
+    jumlah_baca = jumlah_baca + 1
+
+    if terbaca_dan_dipahami == 9:
+        print(f"Buku ke {terbaca_dan_dipahami + 1} belum paham")
     else:
-        jumlah_buku_terbaca = jumlah_buku_terbaca_dan_dipahami + 1
-        print(f'Buku ke {jumlah_buku_terbaca} sudah dibaca dan dipahami')
+        terbaca_dan_dipahami = terbaca_dan_dipahami + 1
+        print(f'Buku ke {terbaca_dan_dipahami} sudah dibaca dan dipahami')
 
-print(f'Jumlah buku yang sudah dibaca{jumlah_buku_terbaca_dan_dipahami}')
-if jumlah_buku_terbaca_dan_dipahami == jumlah_buku:
+print(f'Jumlah buku yang sudah dibaca dan dipahami {terbaca_dan_dipahami}')
+if terbaca_dan_dipahami == jumlah_buku:
     print('Bu, semua buku sudah dibaca dan dipahami')
 else:
     print(f'Bu, tidak semua buku bisa dipahami.'
-          f' budi hanya bisa memahami {jumlah_buku} buku')
-'''
+          f' budi hanya bisa memahami {terbaca_dan_dipahami} buku')
