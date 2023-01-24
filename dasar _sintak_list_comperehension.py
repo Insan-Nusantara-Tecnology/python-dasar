e7d64a26f4407b03dcecab9a9d0f48298a3777d9
Fikri Muza <fikrimuzadi@gmail.com>
2023-01-24 19:10:16 +0700

    comperehension

--- dasar _sintak_list_comperehension.py
+++ dasar _sintak_list_comperehension.py
@@ -28,19 +28,30 @@ daftar_buku_baru = daftar_buku[:]
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
-    print("\nMembuat list baru")
+
+print("\nMembuat list baru")
 del daftar_buku[:]
 for i in range(0, len(daftar_buku_baru)):
     print(daftar_buku_baru[i])
 
 print("\nMembuat list baru dengan comperehension: ganjil")
-daftar_buku = ["Seven Habits", "How to Influence People", "First Things First", "4DX"]
+daftar_buku = ["1 Seven Habits", "2 How to Influence People", "3 First Things First", "4 4DX"]
 daftar_buku_baru = daftar_buku[0::2]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
 
 print("\nMembuat list baru dengan comperehension: genap")
-daftar_buku = ["Seven Habits", "How to Influence People", "First Things First", "4DX"]
+daftar_buku = ["1 Seven Habits", "2 How to Influence People", "3 First Things First", "4 4DX"]
 daftar_buku_baru = daftar_buku[1::2]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print("\nMembuat list baru dengan comperehension: buang ujing")
+daftar_buku = ["1 Seven Habits", "2 How to Influence People", "3 First Things First", "4 4DX"]
+daftar_buku_baru = daftar_buku[1:-1:2]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
+
+print("\nMembuat list baru dengan comperehension: ganjil")
+daftar_buku = ["1 Seven Habits", "2 How to Influence People", "3 First Things First", "4 4DX"]
+print(daftar_buku[0::2])
